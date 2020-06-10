288ab5cf98ff05bdffdae410ca9546bcd2c3ecd6
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2020-06-10 18:54:03 +0300

    Add testing of ceaser cipher

--- practice.js
+++ practice.js
@@ -14,4 +14,15 @@ export const calculator =  {
   divide: (a, b) => {return a / b;},
 }
 
+export const caesar = string => string.replace(/([a-z])/ig, ( char => {
+  let charNumber = char.charCodeAt(0)
+  charNumber === 122 //'z'
+    ? charNumber = 97 // 'Z'
+    : charNumber === 90 //'z' => 'a'
+      ? charNumber = 65 //'Z' => 'A'
+      : charNumber += 1
+    return String.fromCharCode(charNumber)
+  }) );
+
+
 
--- practice.test.js
+++ practice.test.js
@@ -1,5 +1,5 @@
 
-import { capitalize, calculator, reverse } from './practice'
+import { capitalize, caesar, calculator, reverse } from './practice'
 
 
 test(`capitalize 'now' to equal 'Now'`, () => {
@@ -25,4 +25,32 @@ test('Multipy 4 times 3 to equal 12', () => {
 
 test('Divide 28 by 4 to equal 7', () => {
   expect(calculator.divide(28,4)).toBe(7);
+});
+
+
+describe('caesar', () => {
+  it('encrypts a lower case letter', () => {
+      expect(caesar('a')).toBe('b');
+  });
+  it('encrypts a lower case letter', () => {
+    expect(caesar('d')).toBe('e');
+  });
+  it('encrypts an Upper case letter', () => {
+    expect(caesar('F')).toBe('G');
+  });
+  it('wraps z to a in lower case', () => {
+    expect(caesar('z')).toBe('a');
+  });
+  it('wraps Z to A in upper case', () => {
+    expect(caesar('Z')).toBe('A');
+  });
+  it('encrypts words', () => {
+    expect(caesar('Chicken')).toBe('Dijdlfo');
+  });
+  it('encrypts sentences', () => {
+    expect(caesar('a cat a hat')).toBe('b dbu b ibu');
+  });
+  it(`doesn't encrypt puctionation like ' and .`, () => {
+    expect(caesar('Don\'t be a hero')).toBe('Epo\'u cf b ifsp');
+  });
 });
