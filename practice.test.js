6c19a5d4bcb48d5e3e85f1f645e2ae6580c9483d
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2020-06-10 18:13:42 +0300

    Edit subtract feature from test practice

--- practice.test.js
+++ practice.test.js
@@ -15,4 +15,6 @@ test('adds 2 plus 2 to equal 4', () => {
   expect(calculator.add(2,2)).toBe(4);
 });
 
-
+test('subtract 4 minus 3 to equal 1', () => {
+  expect(calculator.subtract(4,3)).toBe(1);
+});
